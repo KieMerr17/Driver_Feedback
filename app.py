111f06f56df18ec3b3cb1481552e55325eefc027
KieMerr17 <kieran.merrett@hotmail.com>
2024-04-14 18:06:52 +0000

    add page one general information

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 # load pages scripts
+from app_pages.page_one_information import site_information
 from app_pages.page_two_feedback_generator import feedback_page
 
 app = MultiPage(app_name="Driver Feedback")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
+app.add_page("Information", site_information)
 app.add_page("Feedback Generator", feedback_page)
 
 app.run()  # Run the app
---
+++ app_pages/page_one_information.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+
+
+def site_information():
+    st.write("#### Use this site to quickly help you generate feedback for your training sessions")
+    st.write("##### Steps:")
+    st.write("- Open up the side bar and select 'Feedback Generator'")
+    st.write("- Select your drivers name, the type of feedback you require (Positive/Constructive),")
+    st.write("- which routes you have completed.")
+    st.write("- what positive points were observed during todays training, what learning points were discussed to improve on.")
+    st.write("- clicke the generate button.")
+    st.write("Your feedback will be displayed in a text box below for you to review. This can be edited if you require before copying into your destination")
+
+
+
+
+
