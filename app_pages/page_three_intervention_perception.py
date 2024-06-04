87ddfc038b9c5d9023a03d14f219ba0900713be6
Kieran Merrett <116359190+KieMerr17@users.noreply.github.com>
2024-06-04 20:52:43 +0100

    remove html embedd and add streamlit video

--- app_pages/page_three_intervention_perception.py
+++ app_pages/page_three_intervention_perception.py
@@ -10,13 +10,19 @@ class SessionState:
     def __init__(self):
         self.timer_running = False
         self.start_time = None
+        self.selected_video = None
+        self.select_new_video = True
 
 session_state = SessionState()
 
+def select_video():
+    if session_state.select_new_video:
+        session_state.selected_video = random.choice(videos)
+        session_state.select_new_video = False
+    return session_state.selected_video
 
 def intervention_perception():
-    # Randomly select a video and format it to .mp4
-    selected_video_dict = random.choice(videos)
+    selected_video_dict = select_video()
     selected_video_key = list(selected_video_dict.keys())[0]
     intervention_start_time, intervention_end_time = selected_video_dict[selected_video_key]
     video_format = list(selected_video_dict.keys())[0] + ".mp4"
@@ -27,34 +33,26 @@ def intervention_perception():
     st.write(selected_video_key)
 
     # Directory of all videos
-    media_dir = "/workspace/Driver_Feedback/media"
+    media_dir = "/Users/kieranmerrett/Driver_Feedback/Driver_Feedback/media"
 
     if selected_video_key:
         # Load the video
         video_path = os.path.join(media_dir, video_format)
-        video_file = open(video_path, "rb").read()
 
-        # Display the video
-        video_id = "my_video"
-        video_data = base64.b64encode(video_file).decode("utf-8")
-        video_html = f"""
-        <video id="{video_id}" controls autoplay width=640 height=360>
-            <source src="data:video/mp4;base64,{video_data}" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-        """
-        st.markdown(video_html, unsafe_allow_html=True)
+        # Display the video with autoplay
+        st.video(video_path, autoplay=True)
 
         if not session_state.timer_running:
             session_state.start_time = time.time()
             session_state.timer_running = True
 
         elapsed_time = time.time() - session_state.start_time
+
         button_slot = st.button("Intervene")
 
         # Actions on clicking 'Intervene'
         if button_slot:
-            intervention_time = elapsed_time - 3
+            intervention_time = elapsed_time
             session_state.timer_running = False
             
             st.write(round(intervention_time, 2))
@@ -66,6 +64,9 @@ def intervention_perception():
                 st.write("Correct Intervention")
             elif intervention_time > intervention_end_time:
                 st.write("Your Intervention was too late")
+
+            # Allow selecting a new video
+            session_state.select_new_video = True
 
     else:
         st.write("No video files found in the /media directory.")
